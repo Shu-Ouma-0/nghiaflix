36b3091be12a20c2fcc8a4467da6e2cdc1f91333
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-07-19 11:11:06 +0700

    create profiles page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -28,11 +28,8 @@ const Auth = () => {
             await signIn('credentials', {
                 email,
                 password,
-                redirect: false,
-                callbackUrl: '/'
+                callbackUrl: '/profiles'
             });
-
-            router.push('/');
         } catch (error) {
             console.log(error);
         }
@@ -94,7 +91,7 @@ const Auth = () => {
                     </button>
                     <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                         <div
-                            onClick={() => signIn('google', {callbackUrl: '/' })}
+                            onClick={() => signIn('google', {callbackUrl: '/profiles' })}
                             className="
                                 w-10
                                 h-10
@@ -111,7 +108,7 @@ const Auth = () => {
                             <FcGoogle size={30} />
                         </div>
                         <div
-                            onClick={() => signIn('github', {callbackUrl: '/' })}
+                            onClick={() => signIn('github', {callbackUrl: '/profiles' })}
                             className="
                                 w-10
                                 h-10
