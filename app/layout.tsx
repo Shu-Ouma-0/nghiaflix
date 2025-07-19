bafce4b32f9cb319b76dda43f958640cfad5966d
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-07-19 11:09:16 +0700

    protection route and when click in Login button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+
+import { Providers } from "./providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,26 +1,26 @@
-import { NextApiRequest } from "next";
-import { getSession } from "next-auth/react";
-
+// lib/serverAuth.ts
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 import prismadb from '@/lib/prsimadb';
 
-const serverAuth = async (req: NextApiRequest) => {
-    const session = await getSession({ req });
+const serverAuth = async () => {
+  const session = await getServerSession(authOptions);
 
-    if (!session?.user?.email) {
-        throw new Error('Not signed in');
-    }
+  if (!session?.user?.email) {
+    throw new Error("Not signed in");
+  }
 
-    const currentUser = await prismadb.user.findUnique({
-        where: {
-            email: session.user.email,
-        }
-    });
+  const currentUser = await prismadb.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
 
-    if (!currentUser) {
-        throw new Error('Not signed in');
-    }
+  if (!currentUser) {
+    throw new Error("Not signed in");
+  }
 
-    return { currentUser };
+  return { currentUser };
 };
 
 export default serverAuth;
