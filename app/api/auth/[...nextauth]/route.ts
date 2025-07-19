3da73581d498696b068758827f8c0eec80fbad33
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-07-19 11:45:22 +0700

    Revert "Merge pull request #4 from Shu-Ouma-0/gg-git-login"
    This reverts commit 47a739128a7d49224f8afe604c9741e23ba2916b, reversing
    changes made to 30fda5748f1afaddbc35db6caf53e20fc46e4a86.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,24 +2,11 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from 'bcrypt';
 
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import prismadb from '@/lib/prsimadb'
 
 const handler = NextAuth({
 //export default NextAuth({
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID || '',
-            clientSecret: process.env.GITHUB_SECRET || ''
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
-        }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
@@ -66,7 +53,6 @@ const handler = NextAuth({
         //error: '/auth/error',
     },
     debug: process.env.NODE_ENV == 'development',
-    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt',
     },
