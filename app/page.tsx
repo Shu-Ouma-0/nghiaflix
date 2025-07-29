2da3d5938214fe68aea0cc1c494df0ea08bd49bf
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-07-29 16:22:24 +0700

    create navbar and navbar items

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { signOut } from "next-auth/react";
+import Navbar from "@/components/Navbar";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -12,12 +13,8 @@ export default async function Home() {
   }
 
   return (
-    <div>
-      <h1 className="text-4xl text-green-500">Nghiaflix</h1>
-      <p className="text-white">Logged in as : {session?.user?.name}</p>
-      <form action="/api/auth/signout" method="POST">
-        <button className="h-10 w-full bg-white">Logout</button>
-      </form>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
