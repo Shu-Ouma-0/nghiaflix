8aec4d6e7b8cb1465f8f5b6cc5cbcd7d27a97228
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-08-03 18:51:45 +0700

    add image domains to use Image from next/image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'resizing.flixster.com',
+      'upload.wikimedia.org',
+      'www.davidrevoy.com',
+      // Nếu còn ảnh từ các domain khác, thêm vào đây
+    ],
+  },
 };
 
 export default nextConfig;
