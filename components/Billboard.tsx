f252292169e184e4006d3b8ce56f03d60dd6bb1c
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-08-03 12:18:59 +0700

    create and add new hooks to Billboard.tsx (useInfoModal)

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import useBillboard from "@/hooks/useBillboard";
-import React from "react";
+import React, { useCallback } from "react";
 
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import PlayButton from "./PlayButton";
+import useInfoModal from "@/hooks/useInfoModal";
 
 const Billboard = () => {
     const { data } = useBillboard();
+    const { openModal } = useInfoModal();
+
+    const handleOpenModal = useCallback(() => {
+        openModal(data?.id);
+    }, [openModal, data?.id]);
 
     return (
         <div className="relative h-[56.25vw]">
@@ -28,12 +34,13 @@ const Billboard = () => {
                 <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
                     {data?.title} 
                 </p>
-                <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
+                <p className="text-white text-xs md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
                     {data?.description}
                 </p>
                 <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
                     <PlayButton movieId={data?.id} />
                     <button
+                        onClick={handleOpenModal}
                         className="
                             bg-white
                             text-white
