5991ca84cbcd81c14bc9a60b0dd0b8fe37f2be33
Shu-Ouma-0 <phamnghia1605@gmail.com>
2025-08-03 12:22:16 +0700

    change hard code Username to data fetch from databse (user table)

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { signOut } from "next-auth/react";
 import React from "react";
 
@@ -8,17 +9,19 @@ interface AccountMenuProps {
 const AccountMenu: React.FC<AccountMenuProps> = ({
     visible
 }) => {
+    const { data } = useCurrentUser();
+
     if (!visible) {
         return null;
     }
 
     return(
-        <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
+        <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex rounded-lg">
             <div className="flex flex-col gap-3">
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/batman.jpg" alt="" />
                     <p className="text-white text-sm group-hover/item:underline">
-                        Username
+                        {data?.name}
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
